2779877f4eead56f738eac8053ef7c4ffbce18d9
Abhijeerauthan111111 <104996007+Abhijeerauthan111111@users.noreply.github.com>
2025-02-10 10:37:53 +0530

    made little changes which i hac=ve to undo later

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 
-import ValentinePage from './components/valentine'
+// import ValentinePage from './components/valentine'
 
 function App() {
  
 
   return (
     <>
-     <div className='bg-pink-100 px-3 py-8'>
+    <div>
+     {/* <div className='bg-pink-100 px-3 py-8'> */}
     
-    <ValentinePage/>
+
+     <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl text-white font-bold">What's up my Nigger!</h1>
+    </div>
+     
+
+    
+    {/* <ValentinePage/> */}
     </div>
 
    
