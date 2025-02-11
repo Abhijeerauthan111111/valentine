5a0fba82df15bca4173f8f5468147497a5886814
Abhijeerauthan111111 <104996007+Abhijeerauthan111111@users.noreply.github.com>
2025-02-11 17:57:16 +0530

    click anywhere to get valentine window

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
     <>
       <div>
         {!niggtext ? (
-          <div>
-            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+          <div onClick={()=>setniggtext(true)}>
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" ></div>
             <div className="flex justify-center items-center h-screen">
-              <button className="text-4xl text-white font-bold" onClick={()=>setniggtext(true) }>What's up my Nigger!</button>
+              <div className="text-4xl text-white font-bold" onClick={()=>setniggtext(true) }>What's up my Nigger!</div>
             </div>
           </div>
         ) : (
